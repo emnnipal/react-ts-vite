ef33b5a3d86e8e2c1b9e0371205f86496640768d
Emmanuel Nipal <emnnipal@gmail.com>
2022-01-30 18:06:16 +0800

    test: validations

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => (
         Edit <code>src/App.tsx</code> and save to reload
       </p>
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
+        Learan React
       </a>
     </header>
   </div>
