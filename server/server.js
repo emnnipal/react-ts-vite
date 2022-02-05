2d8fce34c705b8629f4f07cf44a6b1d0301b1060
Emmanuel Nipal <emnnipal@gmail.com>
2022-02-05 18:24:28 +0800

    feat: enhancements (#10)
    * feat: add naming convention for types, enum and class
    
    * feat: add helmet js security policy
    
    * chore: add more examples for content security policy

--- server/server.js
+++ server/server.js
@@ -9,7 +9,38 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'default-src': [
+          "'self'",
+          /* Whitelist sites here, example below  */
+          'https://iuhzd7tbb9.execute-api.us-west-1.amazonaws.com',
+          'https://98jpgdm7na.execute-api.us-west-1.amazonaws.com',
+        ],
+        /* More examples below */
+        // 'script-src': [
+        //   "'self'",
+        //   'https://edge.marker.io',
+        //   'https://api.marker.io',
+        //   "'unsafe-inline'",
+        // ],
+        // 'img-src': [
+        //   "'self'",
+        //   'https://edge.marker.io',
+        //   'https://api.marker.io',
+        //   "http://www.w3.org/2000/svg'%3E",
+        // ],
+        // 'frame-src': ["'self'", 'https://app.marker.io'],
+      },
+    },
+    referrerPolicy: {
+      policy: 'no-referrer-when-downgrade',
+    },
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+  })
+);
 app.use(express.json());
 app.use(
   '/',
