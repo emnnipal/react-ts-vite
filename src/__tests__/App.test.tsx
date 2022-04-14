cc5225e64dea1935035e0db5be828f8db6eeed60
Emmanuel Nipal <emnnipal@gmail.com>
2022-04-14 21:58:37 +0800

    fix: upgrade pnpm github action version

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 test('renders learn react link', () => {
+  // TODO: change to createRoot to adhere react 18 changes
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
