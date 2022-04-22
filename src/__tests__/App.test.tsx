d0573906e5c2d321b8ad4d5b7bb06bedaa5576f9
Emmanuel Nipal <emnnipal@gmail.com>
2022-04-22 13:28:36 +0800

    fix: resolve unmet peer dependencies

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,8 @@
 import App from '../App';
 
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 
 test('renders learn react link', () => {
-  // TODO: change to createRoot to adhere react 18 changes
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
