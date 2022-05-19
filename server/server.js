ad7c9403d841a4292251279b9941d2cc0671d1d1
Emman Nipal <emnnipal@gmail.com>
2022-05-19 17:15:16 +0800

    chore: remove unused helmet js in server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
@@ -9,38 +9,39 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        'default-src': [
-          "'self'",
-          /* Whitelist sites here, example below  */
-          'https://iuhzd7tbb9.execute-api.us-west-1.amazonaws.com',
-          'https://98jpgdm7na.execute-api.us-west-1.amazonaws.com',
-        ],
-        /* More examples below */
-        // 'script-src': [
-        //   "'self'",
-        //   'https://edge.marker.io',
-        //   'https://api.marker.io',
-        //   "'unsafe-inline'",
-        // ],
-        // 'img-src': [
-        //   "'self'",
-        //   'https://edge.marker.io',
-        //   'https://api.marker.io',
-        //   "http://www.w3.org/2000/svg'%3E",
-        // ],
-        // 'frame-src': ["'self'", 'https://app.marker.io'],
-      },
-    },
-    referrerPolicy: {
-      policy: 'no-referrer-when-downgrade',
-    },
-    crossOriginResourcePolicy: { policy: 'cross-origin' },
-  })
-);
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         'default-src': [
+//           "'self'",
+//           /* Whitelist sites here, example below  */
+//           'https://iuhzd7tbb9.execute-api.us-west-1.amazonaws.com',
+//           'https://98jpgdm7na.execute-api.us-west-1.amazonaws.com',
+//         ],
+//         /* More examples below */
+//         // 'script-src': [
+//         //   "'self'",
+//         //   'https://edge.marker.io',
+//         //   'https://api.marker.io',
+//         //   "'unsafe-inline'",
+//         // ],
+//         // 'img-src': [
+//         //   "'self'",
+//         //   'https://edge.marker.io',
+//         //   'https://api.marker.io',
+//         //   "http://www.w3.org/2000/svg'%3E",
+//         // ],
+//         // 'frame-src': ["'self'", 'https://app.marker.io'],
+//       },
+//     },
+//     referrerPolicy: {
+//       policy: 'no-referrer-when-downgrade',
+//     },
+//     crossOriginResourcePolicy: { policy: 'cross-origin' },
+//   })
+// );
+
 app.use(express.json());
 app.use(
   '/',
