01ab5b937bec95ec6ab8ba18a6c37234a5ad7faa
Emman Nipal <emnnipal@gmail.com>
2022-06-04 16:32:05 +0800

    fix: change handling of env variables to import.meta.env

---
+++ src/shared/configs/app.ts
@@ -0,0 +1,5 @@
+export const { MODE } = import.meta.env;
+
+export const ENV = import.meta.env.VITE_ENV || 'development';
+export const API_URL = import.meta.env.VITE_API_URL;
+export const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
